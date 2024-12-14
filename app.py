758c9051cd641b567a5164699d9b6b626fc63099
Mohamed Waleed <mo.waleed92@gmail.com>
2024-12-14 19:55:46 +0200

    inital commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from turtledemo.penrose import start
+
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car already started")
+        else:
+         started = True
+         print("car started ready to go ...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!!")
+        else:
+            started = False
+            print("car stopped !!")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to exit
+          """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry I don't understand that")
